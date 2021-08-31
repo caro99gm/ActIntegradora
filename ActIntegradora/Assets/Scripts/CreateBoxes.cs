0b9d59dc18da2bb2f0483feb796450ec81fade9a
caro99gm <caro99gm@gmail.com>
2021-08-31 15:21:07 -0500

    Implement generate boxes

---
+++ ActIntegradora/Assets/Scripts/CreateBoxes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBoxes : MonoBehaviour
+{
+    public GameObject box;
+
+    public int NumBoxes = 15;
+    List<GameObject> ArrBoxes;
+
+    void Start()
+    {
+        ArrBoxes = new List<GameObject>();
+        for(int i = 0; i < NumBoxes; i++){
+            float x = Random.Range(-18 , -2);
+            float y = 0.32f;
+            float z = Random.Range(-12, 12);   
+
+            Instantiate(box, new Vector3(x,y,z), Quaternion.Euler(0,0,0));   
+        }
+    }
+}
