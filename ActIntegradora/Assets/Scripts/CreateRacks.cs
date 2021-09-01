6157f50285ff1701a8e2c9684cb097f620ce7edf
caro99gm <caro99gm@gmail.com>
2021-08-31 21:34:12 -0500

    Fix assets and racks

--- ActIntegradora/Assets/Scripts/CreateRacks.cs
+++ ActIntegradora/Assets/Scripts/CreateRacks.cs
@@ -6,29 +6,33 @@ public class CreateRacks : MonoBehaviour
 {
     public GameObject Rack;
 
-    public int NumRacks = 30;
+    public int NumRacks = 28;
     List<GameObject> ArrRacks;
 
     void Start()
     {
         ArrRacks = new List<GameObject>();
         for(int i = 0; i < NumRacks; i++){
-            float x = Random.Range(-18 , -2);
-            float y = 0;
+            float y = 0.02392491f;
             float z = Random.Range(-12, 12);
 
-            if(i < NumRacks/9){
-                Instantiate(Rack, new Vector3(x,y,z), Quaternion.Euler(0,0,0));
+            float x1 = Random.Range(-3 , -2);
+            float x2 = Random.Range(-8 , -7);
+            float x3 = Random.Range(-15 , -14);
+            float x4 = Random.Range(-20 , -19);
+
+            if(i < 7){
+                Instantiate(Rack, new Vector3(x1,y,z), Quaternion.Euler(0,0,0));
             }
-            else if(i < NumRacks/6){
-                Instantiate(Rack, new Vector3(x,y,z), Quaternion.Euler(0,90,0));
-            } 
-            else if(i < NumRacks/3){
-                Instantiate(Rack, new Vector3(x,y,z), Quaternion.Euler(0,270,0));
-            }  
-            else{
-                Instantiate(Rack, new Vector3(x,y,z), Quaternion.Euler(0,180,0));
-            }           
+            else if(i < 14){
+                Instantiate(Rack, new Vector3(x2,y,z), Quaternion.Euler(0,0,0));
+            }
+            else if(i < 21){
+                Instantiate(Rack, new Vector3(x3,y,z), Quaternion.Euler(0,0,0));
+            }
+            else if(i < 28){
+                Instantiate(Rack, new Vector3(x4,y,z), Quaternion.Euler(0,0,0));
+            }            
         }
     }
 }
