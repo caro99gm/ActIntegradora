637c8cf56a760650a3d2aeee4d253f9cbe8927bc
caro99gm <caro99gm@gmail.com>
2021-08-31 22:21:39 -0500

    Add collision

---
+++ ActIntegradora/Assets/Scripts/DetectObjects.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.name == "Box"){
+            print("caja");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+//poner script en el robot
+
+//robot random
+
+//robot
+
+
+//cajas
+    //agarrala -> child
+
+//estantes
+    //deja la caja
+
+
