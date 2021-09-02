b5baa02a12aec593fdfc7555c415a90b0cc636f3
caro99gm <caro99gm@gmail.com>
2021-09-02 00:31:14 -0500

    Start implementing collisions

--- ActIntegradora/Assets/Scripts/CreateRacks.cs
+++ ActIntegradora/Assets/Scripts/CreateRacks.cs
@@ -6,7 +6,7 @@ public class CreateRacks : MonoBehaviour
 {
     public GameObject Rack;
 
-    public int NumRacks = 28;
+    public int NumRacks = 32;
     List<GameObject> ArrRacks;
 
     void Start()
@@ -14,23 +14,23 @@ public class CreateRacks : MonoBehaviour
         ArrRacks = new List<GameObject>();
         for(int i = 0; i < NumRacks; i++){
             float y = 0.02392491f;
-            float z = Random.Range(-12, 12);
+            float z = Random.Range(-10, 11);
 
             float x1 = Random.Range(-3 , -2);
             float x2 = Random.Range(-8 , -7);
             float x3 = Random.Range(-15 , -14);
             float x4 = Random.Range(-20 , -19);
 
-            if(i < 7){
+            if(i < 8){
                 Instantiate(Rack, new Vector3(x1,y,z), Quaternion.Euler(0,0,0));
             }
-            else if(i < 14){
+            else if(i < 16){
                 Instantiate(Rack, new Vector3(x2,y,z), Quaternion.Euler(0,0,0));
             }
-            else if(i < 21){
+            else if(i < 24){
                 Instantiate(Rack, new Vector3(x3,y,z), Quaternion.Euler(0,0,0));
             }
-            else if(i < 28){
+            else if(i < 32){
                 Instantiate(Rack, new Vector3(x4,y,z), Quaternion.Euler(0,0,0));
             }            
         }
--- ActIntegradora/Assets/Scripts/DetectObjects.cs
+++ ActIntegradora/Assets/Scripts/DetectObjects.cs
@@ -4,29 +4,71 @@ using UnityEngine;
 
 public class DetectObjects : MonoBehaviour
 {
+    public int check = 0;
+    public GameObject Box1;
+    public GameObject Box2;
+    public GameObject Box3;
+    public GameObject Box4;
+    public GameObject Box5;
+
     void OnCollisionEnter(Collision collision){
+        GameObject objeto = collision.gameObject;
+
         if(collision.gameObject.tag == "Box"){
-            Debug.Log("caja");
+            transform.parent = objeto.transform;
+            check = 1;
+            while(collision.gameObject.tag == "LeftWall"){
+                transform.Translate(Vector3.left*Time.deltaTime*10);
+            }
+            while(collision.gameObject.tag == "TopWall" && collision.gameObject.tag == "Rack"){
+                transform.Translate(Vector3.up*Time.deltaTime*10);
+                if(collision.gameObject.tag == "TopWall"){
+                    while(collision.gameObject.tag != "Rack"){
+                        transform.Translate(Vector3.right*Time.deltaTime*10);
+                    }
+                }
+                if(collision.gameObject.tag == "Rack"){
+                    float x = Random.Range(-3 , -2);
+                    float y = 0.02392491f;
+                    float z = Random.Range(-10, 0);
+
+                    if(collision.gameObject.tag == "Rack"){
+                        Instantiate(Box1, new Vector3(x,y,z), Quaternion.Euler(0,0,0));
+                    }
+                    else if(collision.gameObject.tag == "Box1"){
+                        Instantiate(Box2, new Vector3(x,y,z), Quaternion.Euler(0,0,0));
+
+                    }
+                    else if(collision.gameObject.tag == "Box2"){
+                        Instantiate(Box3, new Vector3(x,y,z), Quaternion.Euler(0,0,0));
+
+                    }
+                    else if(collision.gameObject.tag == "Box3"){
+                        Instantiate(Box4, new Vector3(x,y,z), Quaternion.Euler(0,0,0));
+
+                    }
+                    else if(collision.gameObject.tag == "Box4"){
+                        Instantiate(Box5, new Vector3(x,y,z), Quaternion.Euler(0,0,0));
+                    }
+                    else{
+                        print("nada");
+                    }
+                }
+            }
         }
-        if(collision.gameObject.tag == "Rack"){
-            print("rack");
-        }
+
+        
+
         if(collision.gameObject.tag == "Robot"){
+            
             print("robot");
         }
     }
 }
-//poner script en el robot
-
-//robot random
-
-//robot
 
 
-//cajas
-    //agarrala -> child
-
-//estantes
-    //deja la caja
-
-
+//Faltanes:
+//Mover robot
+//Chocar entre robots
+//Obtener posicion en Z para poner el nuevo rack
+//Quitar el rack anterior
